414ce4256455e607d721da0b814dc96f9a2715da
bland_onium <107197514+bland-onium@users.noreply.github.com>
2022-12-02 14:06:15 +0500

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -2,22 +2,52 @@ print("input command(0->float)(1->int): ")
 cmnd = int(input())
 
 if cmnd == 0:
-    print("Now you're turning 10th to 2nd.")
-    print("input number -> ")
-    numb = float(input())
-    print("Please, input length of required base -> "); listlen = int(input())
-    lst = []
-    while (len(lst) < listlen) or (numb != 0):
-        if numb == 0: break
-        if numb * 2 < 1:
-            lst.append('0')
-        elif numb * 2 > 1:
-            lst.append('1')
-        numb = numb * 2
-        if numb > 1: numb = numb - 1
-        if len(lst) == listlen:
-            print(*lst)
-            break
+    print("Input undercommand (0=> 2->10)(1=> 10->2) -> "); undercommand = int(input())
+    print("input your number (after '.' -> "); N = input(); N = N.upper(); N = str(N)
+    if undercommand == 0:
+        fin = 0; degr = -1
+        print("Now you're turning fraction to smth another.")
+        lst = []; numlist = []
+        for i in N:
+            if (i == "1"  ): i = "1"
+            elif (i == "2"): i = "2"
+            elif (i == "3"): i = "3"
+            elif (i == "4"): i = "4"
+            elif (i == "5"): i = "5"
+            elif (i == "6"): i = "6"
+            elif (i == "7"): i = "7"
+            elif (i == "8"): i = "8"
+            elif (i == "9"): i = "9"
+            elif (i == "0"): i = "0"
+            elif ("A" == i): i = " 10"
+            elif ("B" == i): i = " 11"
+            elif ("C" == i): i = " 12"
+            elif ("D" == i): i = " 13"
+            elif ("E" == i): i = " 14"
+            elif ("F" == i): i = " 15"
+            numlist.append(i)
+        while (len(numlist) > 0):
+            fin = fin + int(numlist[0]) * (pow(2, degr))
+            degr = degr - 1
+            print(fin)
+            numlist.pop(0)
+    if undercommand == 1:
+        numb = N
+        print("Now you're turning 10th to 2nd.")
+        print("Please, input length of required base -> ");
+        listlen = int(input())
+        lst = []
+        while (len(lst) < listlen) or (numb != 0):
+            if numb == 0: break
+            if numb * 2 < 1:
+                lst.append('0')
+            elif numb * 2 > 1:
+                lst.append('1')
+            numb = numb * 2
+            if numb > 1: numb = numb - 1
+            if len(lst) == listlen:
+                print(*lst)
+                break
 
 if cmnd == 1:
     fin = 0; degr = 0; n = []
